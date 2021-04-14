adcd2b2bb3c104cf035cbba0c26403d21c74681c
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-04-14 15:35:23 +0530

    Update flatten_the_nestest_list.cpp

--- arrays/flatten_the_nestest_list.cpp
+++ arrays/flatten_the_nestest_list.cpp
@@ -1,3 +1,4 @@
+// link to que - https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3706/
 class NestedIterator {
     
     vector<int> result;
