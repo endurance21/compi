b2e818faa679562e805d79299cfd4c8ad32e0274
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-10-22 01:14:02 +0530

    Update codeforeces_template.cpp

--- codeforeces_template.cpp
+++ codeforeces_template.cpp
@@ -1,18 +1,40 @@
 #include<bits/stdc++.h>
-using namespace std;
 
-void solve(){
-   
-}
+#define v(x) vector<x>
+#define loop(x,n) for(int x = 0; x < n; ++x)
+using namespace std ; 
+
+
 int main(){
-     
-     int t ;
-     cin>>t ;
-     while(t--){
-          solve(); 
-     }
-     
-     
-     
-     
+
+
+	int t = 1 ;; 
+	// cin>>t ;
+	while(t--){
+
+		 int n ;
+		 cin>>n ;
+	 	v(string) vec(n) ;
+
+
+		loop(i,n){
+			cin>>vec[i] ;
+		}
+
+		//  cout<<"hello" ;
+		 loop(x,n){
+			 int count = 0 ;
+			 loop(y,x){
+
+					 if(strcmp(vec[y].c_str(),vec[x].c_str()) < 0 ){
+						 count++ ;
+					 }
+			 }
+
+			 cout<<count <<endl ;
+		 }
+		
+	}
+	return 0 ;
 }
+
